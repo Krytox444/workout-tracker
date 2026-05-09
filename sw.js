4ece3c1da5dc55f968df984c0f59be1176b41705
Krytox444 <jakobduejuhre@hotmail.com>
2026-05-09 16:01:05 +0200

    e

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'workout-tracker-v9';
+const CACHE = 'workout-tracker-v10';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
